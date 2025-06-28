b022b5a505eece07d86fbf4caf7ab1e97a3da324
Abhishek Kumar <abhi.code101@gmail.com>
2025-06-28 14:50:33 +0530

    test: add single number test

--- test/string_calculator_tdd_test.dart
+++ test/string_calculator_tdd_test.dart
@@ -4,4 +4,8 @@ void main() {
   test('returns 0 for empty string', () {
     expect(add(''), 0);
   });
+  test('returns the number itself for a single number', () {
+    expect(add('1'), 1);
+    expect(add('42'), 42);
+  });
 }
