157f8b0f3474b4acca1c5023d2513b81b68635f9
Abhishek Kumar <abhi.code101@gmail.com>
2025-06-28 14:51:15 +0530

    test: add two numbers sum test

---
+++ test/string_calculator_tdd_test.dart
@@ -0,0 +1,15 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('returns 0 for empty string', () {
+    expect(add(''), 0);
+  });
+  test('returns the number itself for a single number', () {
+    expect(add('1'), 1);
+    expect(add('42'), 42);
+  });
+  test('returns the sum for two comma-separated numbers', () {
+    expect(add('1,2'), 3);
+    expect(add('10,5'), 15);
+  });
+}
